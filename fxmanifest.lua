87f7e64a5724e507da68b826734ed87b7260975c
RDX Development <rotherhamjoshua@gmail.com>
2025-12-15 23:31:14 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+
+author 'RDX Development'
+description 'Standalone /dv delete-vehicle command (Discord: https://discord.gg/R7JZeBYB3M)'
+version '1.0.0'
+
+client_scripts { 'client.lua' }
+server_scripts { 'server.lua' }
